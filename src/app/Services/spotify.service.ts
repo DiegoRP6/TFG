da338a191f5346cc187ad02d08e499c2f7a1eeb4
Diego <drogriguez@nervia.es>
2024-11-26 15:56:07 +0100

    Minor fix

--- src/app/Services/spotify.service.ts
+++ src/app/Services/spotify.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { debounceTime } from 'rxjs/operators';
-import { RelatedArtist } from '../modules/relatedArtist.model';
 
 @Injectable({
   providedIn: 'root'
--- src/app/artista/artista.component.ts
+++ src/app/artista/artista.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SpotifyService } from '../Services/spotify.service';
-import { RelatedArtist } from '../modules/relatedArtist.model';
 
 @Component({
   selector: 'app-artista',
